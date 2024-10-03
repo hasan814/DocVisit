967028b1b3cb15ea19e6eb81236d728a6237dbd8
hasan814 <h.mousavi910@gmail.com>
2024-10-03 21:59:50 +0330

    Added Pages

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import AboutPage from "../components/templates/AboutPage";
+
+const About = () => {
+  return (
+    <div>
+      <AboutPage />
+    </div>
+  );
+};
+
+export default About;
---
+++ client/src/pages/Doctors.jsx
@@ -0,0 +1,11 @@
+import DoctorsPage from "../components/templates/DoctorsPage";
+
+const Doctors = () => {
+  return (
+    <div>
+      <DoctorsPage />
+    </div>
+  );
+};
+
+export default Doctors;
